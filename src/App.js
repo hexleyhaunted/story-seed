48560746d92a25ea7bbbdd9ec6ccbb9a9bca0a34
James Dawson <63356869+jamesd-dev@users.noreply.github.com>
2023-10-11 22:35:08 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import imagesJson from './images.json';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [selectedImageKeys, setSelectedImageKeys] = useState([]);
+  let availableImages = Object.keys(imagesJson.images);
+
+  useEffect(() => {
+    selectNewImages();
+  }, []);
+
+  function selectNewImages() {
+    if(availableImages.length < 3) {
+      availableImages = Object.keys(imagesJson.images);
+    }
+
+    let selectedKeys = [];
+
+    while (selectedKeys.length < 3) {
+      const newKey = getRandomItem(availableImages);
+      availableImages = availableImages.filter(i => i !== newKey);
+      selectedKeys.push(newKey);
+    }
+
+    setSelectedImageKeys(selectedKeys);
+  }
+
+  function getRandomItem(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={"image-container"}>
+        {selectedImageKeys.map(i => {
+          return <img src={imagesJson.images[i]} alt={i} className={"image"}/>
+        })}
+      </div>
+      <button className={"button"} onClick={selectNewImages}>shuffle</button>
     </div>
   );
 }
